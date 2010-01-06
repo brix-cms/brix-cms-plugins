d0a89198d8c7d2becaef112d2d0d894eca00cfbf
Igor Vaynberg <igor.vaynberg@gmail.com>
2010-01-06 00:34:18 +0000

    extension points for markup editor factories

---
+++ brix-markupeditor-codepress/src/main/java/brix/codepress/CodePressMarkupEditorFactory.java
@@ -0,0 +1,24 @@
+package brix.codepress;
+
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+import brix.plugin.site.page.admin.MarkupEditorFactory;
+
+public class CodePressMarkupEditorFactory implements MarkupEditorFactory
+{
+
+    public TextArea<String> newEditor(String id, IModel<String> markup)
+    {
+        TextArea<String> editor = new TextArea<String>(id, markup);
+        editor.add(new CodePressEnabler("html", true));
+        return editor;
+    }
+
+    public IModel<String> newLabel()
+    {
+        return new Model<String>("CodePress");
+    }
+
+}
---
+++ brix-markupeditor-tinymce/src/main/java/brix/tinymce/TinyMceMarkupEditorFactory.java
@@ -0,0 +1,24 @@
+package brix.tinymce;
+
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+import brix.plugin.site.page.admin.MarkupEditorFactory;
+
+public class TinyMceMarkupEditorFactory implements MarkupEditorFactory
+{
+
+    public TextArea<String> newEditor(String id, IModel<String> markup)
+    {
+        TextArea<String> editor = new TextArea<String>(id, markup);
+        editor.add(new TinyMceEnabler());
+        return editor;
+    }
+
+    public IModel<String> newLabel()
+    {
+        return new Model<String>("TinyMCE WYSIWYG");
+    }
+
+}
