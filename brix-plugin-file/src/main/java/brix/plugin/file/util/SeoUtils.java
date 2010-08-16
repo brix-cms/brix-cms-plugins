a9dd0b4284975a0c3bcbeea1eb9bc0bd53f426e7
wickeria <wickeria@gmail.com>
2010-08-16 20:47:29 +0000

    cleanup

--- brix-plugin-file/src/main/java/brix/plugin/file/util/SeoUtils.java
+++ brix-plugin-file/src/main/java/brix/plugin/file/util/SeoUtils.java
@@ -21,18 +21,10 @@ import java.text.Normalizer;
 public class SeoUtils {
 	private static final int MAX_URL_LENGTH = 30;
 
-	public static final String PICTURE_PARAM_KEY = "image";
-
-	public static final String SELECTED_PARAM_KEY = "selectedBy";
-
-	public static final String RACE_PARAM_KEY = "race";
-	public static final String RACE_EDIT_PARAM_KEY = "raceEdit";
-
 	public static String normalizeValue(String value) {
 		return normalizeValue(value, MAX_URL_LENGTH);
 	}
 
-	// TODO: toto bude asi lepsi:
 	// String decomposed = java.text.Normalizer.normalize(string,
 	// Normalizer.Form.NFD);
 	// return decomposed.replaceAll("p{InCombiningDiacriticalMarks}+", "");
@@ -41,17 +33,14 @@ public class SeoUtils {
 		String inputValue = value;
 		String ret = value;
 		if (inputValue != null) {
-			/* odstran carky hacky */
-			ret = Normalizer.normalize(inputValue.subSequence(0, inputValue.length()), Normalizer.Form.NFKD)
-					.replaceAll("[^\\p{ASCII}]+", "");
 
-			/* vsechny NE-alfanumericke znaky nahrad pomlckou */
+			ret = Normalizer.normalize(inputValue.subSequence(0, inputValue.length()), Normalizer.Form.NFKD).replaceAll("[^\\p{ASCII}]+",
+					"");
+
 			ret = ret.replaceAll("[^a-zA-Z0-9]", "-");
 
-			/* opakujici pomlcky zgrupuj do jedne */
 			ret = ret.replaceAll("-+", "-");
 
-			/* orizni na velikost textfieldu, pokud to je nutne */
 			if (ret.length() > maxURLLength) {
 				ret = ret.substring(0, maxURLLength);
 			}
@@ -60,27 +49,5 @@ public class SeoUtils {
 		}
 
 		return ret;
-	}
-
-	/**
-	 * use getLastParamId
-	 */
-	@Deprecated
-	public static String separateParamId(String last) {
-		return last.substring(last.lastIndexOf("-") + 1);
-	}
-
-	public static void main(String[] args) {
-		// String str = "123-456";
-		// System.out.println(getFirstParamId(str));
-		// System.out.println(getLastParamId(str));
-	}
-
-	public static String getFirstParamId(String str) {
-		return str.substring(0, str.indexOf("-"));
-	}
-
-	public static String getLastParamId(String last) {
-		return last.substring(last.lastIndexOf("-") + 1);
 	}
 }
