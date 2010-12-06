53cb51bf57c9289505be65df0a374cd0daf2ccda
Brian Topping <topping@codehaus.org>
2010-12-06 04:54:28 +0000

    Component that can make very pretty text entry fields.

--- brix-plugin-article/src/main/java/brix/plugin/article/web/tile/message/MessageListPanel.java
+++ brix-plugin-article/src/main/java/brix/plugin/article/web/tile/message/MessageListPanel.java
@@ -14,11 +14,13 @@
 
 package brix.plugin.article.web.tile.message;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
+import brix.auth.Action.Context;
+import brix.jcr.api.JcrNode;
+import brix.jcr.api.JcrNodeIterator;
+import brix.jcr.wrapper.BrixNode;
+import brix.plugin.article.ArticlePlugin;
+import brix.plugin.file.web.ConfirmAjaxCallDecorator;
+import brix.web.generic.BrixGenericPanel;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.IAjaxCallDecorator;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
@@ -27,19 +29,15 @@ import org.apache.wicket.datetime.PatternDateConverter;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PageableListView;
-import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.util.convert.IConverter;
 
-import brix.auth.Action.Context;
-import brix.jcr.api.JcrNode;
-import brix.jcr.api.JcrNodeIterator;
-import brix.jcr.wrapper.BrixNode;
-import brix.plugin.article.ArticlePlugin;
-import brix.plugin.file.web.ConfirmAjaxCallDecorator;
-import brix.web.generic.BrixGenericPanel;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author wickeria at gmail.com
@@ -50,114 +48,112 @@ public class MessageListPanel extends BrixGenericPanel<BrixNode> {
     private boolean isDiscussion;
 
     public MessageListPanel(String id, IModel<BrixNode> model, final boolean isDiscussion) {
-	super(id, model);
-	this.isDiscussion = isDiscussion;
-	setOutputMarkupId(true);
-	final int messagesPerPage = isDiscussion ? Integer.MAX_VALUE : 10;
-	final PageableListView<Message> listView = new PageableListView<Message>("entries", new EntriesModel(),
-		messagesPerPage) {
-	    private static final long serialVersionUID = 1L;
+        super(id, model);
+        this.isDiscussion = isDiscussion;
+        setOutputMarkupId(true);
+        final int messagesPerPage = isDiscussion ? Integer.MAX_VALUE : 10;
+        final PageableListView<Message> listView = new PageableListView<Message>("entries", new EntriesModel(), messagesPerPage) {
+            private static final long serialVersionUID = 1L;
 
-	    @Override
-	    protected IModel<Message> getListItemModel(IModel<? extends List<Message>> listViewModel, int index) {
-		return new CompoundPropertyModel<Message>(super.getListItemModel(listViewModel, index));
-	    }
+            @Override
+            protected IModel<Message> getListItemModel(IModel<? extends List<Message>> listViewModel, int index) {
+                return new CompoundPropertyModel<Message>(super.getListItemModel(listViewModel, index));
+            }
 
-	    @Override
-	    protected void populateItem(ListItem<Message> item) {
-		item.add(new Label("index"));
-		item.add(new Label("name"));
-		item.add(new Label("title"));
-		item.add(new Label("message"));
-		item.add(new Label("timestamp") {
-		    @Override
-		    public IConverter getConverter(Class<?> type) {
-			return new PatternDateConverter("dd.MM.yyyy HH:mm:ss", false);
-		    }
-		});
-		item.add(new AjaxLink<String>("nodeId") {
-		    private static final long serialVersionUID = 1L;
+            @Override
+            protected void populateItem(ListItem<Message> item) {
+                item.add(new Label("index"));
+                item.add(new Label("name"));
+                item.add(new Label("title"));
+                item.add(new Label("message"));
+                item.add(new Label("timestamp") {
+                    @Override
+                    public IConverter getConverter(Class<?> type) {
+                        return new PatternDateConverter("dd.MM.yyyy HH:mm:ss", false);
+                    }
+                });
+                item.add(new AjaxLink<String>("nodeId") {
+                    private static final long serialVersionUID = 1L;
 
-		    @Override
-		    protected IAjaxCallDecorator getAjaxCallDecorator() {
-			return new ConfirmAjaxCallDecorator();
-		    }
+                    @Override
+                    protected IAjaxCallDecorator getAjaxCallDecorator() {
+                        return new ConfirmAjaxCallDecorator();
+                    }
 
-		    @Override
-		    public void onClick(AjaxRequestTarget target) {
-			BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
-			brixNode.getSession().getNodeByIdentifier(getDefaultModelObjectAsString()).remove();
-			brixNode.save();
-			findParent(PageableListView.class).setModel(new EntriesModel());
-			target.addComponent(MessageListPanel.this);
-		    }
+                    @Override
+                    public void onClick(AjaxRequestTarget target) {
+                        BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
+                        brixNode.getSession().getNodeByIdentifier(getDefaultModelObjectAsString()).remove();
+                        brixNode.save();
+                        findParent(PageableListView.class).setModel(new EntriesModel());
+                        target.addComponent(MessageListPanel.this);
+                    }
 
-		    @Override
-		    public boolean isVisible() {
-			BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
-			BrixNode node = (BrixNode) brixNode.getSession().getNodeByIdentifier(
-				getDefaultModelObjectAsString());
-			return ArticlePlugin.get().canDeleteNode(node, Context.PRESENTATION);
-		    }
-		});
-		item.add(new Label("email") {
-		    @Override
-		    public boolean isVisible() {
-			//only admin can see email
-			BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
-			return ArticlePlugin.get().canDeleteNode(brixNode, Context.PRESENTATION);
-		    }
-		});
-	    }
+                    @Override
+                    public boolean isVisible() {
+                        BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
+                        BrixNode node = (BrixNode) brixNode.getSession().getNodeByIdentifier(getDefaultModelObjectAsString());
+                        return ArticlePlugin.get().canDeleteNode(node, Context.PRESENTATION);
+                    }
+                });
+                item.add(new Label("email") {
+                    @Override
+                    public boolean isVisible() {
+                        //only admin can see email
+                        BrixNode brixNode = (BrixNode) MessageListPanel.this.getDefaultModelObject();
+                        return ArticlePlugin.get().canDeleteNode(brixNode, Context.PRESENTATION);
+                    }
+                });
+            }
 
-	};
-	add(listView);
-	add(new AjaxPagingNavigator("navigator", listView) {
-	    @Override
-	    public boolean isVisible() {
-		    JcrNode tile = (JcrNode) MessageListPanel.this.getModelObject();
-		    JcrNodeIterator entryNodes = tile.getNodes("entry");
-		    return entryNodes.getSize() > messagesPerPage;
-	    }
-	});
+        };
+        add(listView);
+        add(new AjaxPagingNavigator("navigator", listView) {
+            @Override
+            public boolean isVisible() {
+                JcrNode tile = (JcrNode) MessageListPanel.this.getModelObject();
+                JcrNodeIterator entryNodes = tile.getNodes("entry");
+                return entryNodes.getSize() > messagesPerPage;
+            }
+        });
     }
 
     private class EntriesModel extends LoadableDetachableModel<List<Message>> {
 
-	private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-	@Override
-	protected List<Message> load() {
-	    JcrNode tile = (JcrNode) getDefaultModelObject();
-	    JcrNodeIterator entryNodes = tile.getNodes("entry");
-	    ArrayList<Message> entries = new ArrayList<Message>((int) entryNodes.getSize());
+        @Override
+        protected List<Message> load() {
+            JcrNode tile = (JcrNode) getDefaultModelObject();
+            JcrNodeIterator entryNodes = tile.getNodes("entry");
+            ArrayList<Message> entries = new ArrayList<Message>((int) entryNodes.getSize());
 
-	    while (entryNodes.hasNext()) {
-		JcrNode entryNode = entryNodes.nextNode();
-		Message message = new Message();
-		message.setIndex((int) entryNode.getProperty("index").getLong());
-		message.setName(entryNode.getProperty("name").getString());
-		message.setMessage(entryNode.getProperty("message").getString());
-		if (entryNode.hasProperty("timestamp")) {
-		    message.setTimestamp(new Date(entryNode.getProperty("timestamp").getLong()));
-		}
-		if (entryNode.hasProperty("email")) {
-		    message.setEmail(entryNode.getProperty("email").getString());
-		}
-		if (entryNode.hasProperty("title")) {
-		    message.setTitle(entryNode.getProperty("title").getString());
-		}
-		message.setNodeId(entryNode.getIdentifier());
-		entries.add(message);
-	    }
+            while (entryNodes.hasNext()) {
+                JcrNode entryNode = entryNodes.nextNode();
+                Message message = new Message();
+                message.setIndex((int) entryNode.getProperty("index").getLong());
+                message.setName(entryNode.getProperty("name").getString());
+                message.setMessage(entryNode.getProperty("message").getString());
+                if (entryNode.hasProperty("timestamp")) {
+                    message.setTimestamp(new Date(entryNode.getProperty("timestamp").getLong()));
+                }
+                if (entryNode.hasProperty("email")) {
+                    message.setEmail(entryNode.getProperty("email").getString());
+                }
+                if (entryNode.hasProperty("title")) {
+                    message.setTitle(entryNode.getProperty("title").getString());
+                }
+                message.setNodeId(entryNode.getIdentifier());
+                entries.add(message);
+            }
 
-	    if (isDiscussion) {
-		return entries;
-	    } else {
-		Collections.reverse(entries);
-		return entries;
-	    }
-	}
+            if (isDiscussion) {
+                return entries;
+            } else {
+                Collections.reverse(entries);
+                return entries;
+            }
+        }
 
     }
 
